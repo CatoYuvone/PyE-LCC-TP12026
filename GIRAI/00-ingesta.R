2a2bdc515fb44fc4cef339cdffcbd6a744f2d1f3
CatoYuvone <otacyuvo@gmail.com>
2026-04-24 08:21:03 -0300

    fix: Ingesta de datos ahora mediante sheets

--- GIRAI/00-ingesta.R
+++ GIRAI/00-ingesta.R
@@ -1,15 +1,16 @@
 # Instalo los paquetes necesarios (si aún no los tengo instalados)
-install.packages("readxl")
-library(readxl)
+# install.packages("googlesheets4")
 
-
+library(googlesheets4)
 
 # Link al archivo
 url="https://docs.google.com/spreadsheets/d/1Kwl4KByOv8q2kXMsgaO3d5QI3vUQ40RCZJgJHhg5bmE/edit?gid=580479207#gid=580479207"
 
-datos <- read_excel("/GIRAI/data.xlsx", skip = 1)
+# Evito loggeo
+gs4_deauth()
 
 # Leo el archivo y almaceno los datos en un data frame
+datos <- read_sheet(url, sheet = "tp", skip = 1)
 
 # Veo la estructura del dataset
 str(datos)
