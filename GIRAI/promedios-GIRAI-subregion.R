0695d0f72598c3da6abfd667676161dca876cfe7
CatoYuvone <otacyuvo@gmail.com>
2026-04-24 21:17:40 -0300

    feat: Cantidad de países por GIRAI

--- GIRAI/promedios-GIRAI-subregion.R
+++ GIRAI/promedios-GIRAI-subregion.R
@@ -5,3 +5,19 @@ promedios <- aggregate(GIRAI ~ UN_subregion, data = datos, FUN = mean, na.rm = T
 promedios <- promedios[order(-promedios$GIRAI), ]
 
 print(promedios)
+
+
+# ============================================================
+# Descripción gráfica de una variable cuantitativa continua
+# Variable elegida: GIRAI
+# ============================================================
+
+
+ggplot(datos, aes(x = GIRAI)) +
+  geom_histogram(bins = 10) +
+  labs(
+    title = "Distribución del índice GIRAI",
+    subtitle = "Variable cuantitativa continua",
+    x = "GIRAI",
+    y = "Cantidad de países"
+  )
